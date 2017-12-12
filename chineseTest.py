67f14afbeb371e636d2f6cf3d3fa6a0730b67c79
ViterbiDevelopment <wangchaonihao@163.com>
2017-12-12 22:51:03 +0800

    add test

---
+++ chineseTest.py
@@ -0,0 +1,15 @@
+
+ #-*- coding: utf-8 -*-
+
+# import os
+
+# f = open('chinese.text','w+')
+
+# for x in xrange(1,10000):
+# 	f.write('哈哈哈')
+
+# f.close()
+
+testh = '哈哈ha\haa'
+
+print testh.split('\\')[1]
